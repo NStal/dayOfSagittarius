0f736f0ce636e1174535afd3cc0fdfbce9066790
NStal <nstalmail@gmail.com>
2012-03-19 09:06:46 +0800

    add mklink.sh :for recreate symlinks of shared files in windows

---
+++ mklink.sh
@@ -0,0 +1,13 @@
+mkdir server/share/
+cd server/share/
+ln -s ../ship 
+ln -s ../battleFieldSoul.js 
+ln -s ../gameUtil.js 
+ln -s ../protocol.js 
+ln -s ../serverGateway.js 
+ln -s ../util.js 
+ln -s ../world.js 
+cd ../../browser
+
+ln -s ../server/share
+
